2a4b0aa8d9350b59021d41fc3b52286fe4131164
Raquel Cristóvão <rmcristovao@gmail.com>
2016-10-15 22:56:02 +0100

    spaceship com cones

--- Projecto_v1/Proj_v1/Spaceship.cpp
+++ Projecto_v1/Proj_v1/Spaceship.cpp
@@ -13,9 +13,22 @@ Spaceship::Spaceship(int _objId, int* addedToId, float _x, float _y, float _z) :
 	memcpy(mesh[objectId].mat.emissive, emissive, 4 * sizeof(float));
 	mesh[objectId].mat.shininess = shininess;
 	mesh[objectId].mat.texCount = texcount;
-	//createQuad(1.5f,1.5f);
+	memcpy(mesh[objectId + 1].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objectId + 1].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objectId + 1].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objectId + 1].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objectId + 1].mat.shininess = shininess;
+	mesh[objectId + 1].mat.texCount = texcount;
+	memcpy(mesh[objectId + 2].mat.ambient, amb, 4 * sizeof(float));
+	memcpy(mesh[objectId + 2].mat.diffuse, diff, 4 * sizeof(float));
+	memcpy(mesh[objectId + 2].mat.specular, spec, 4 * sizeof(float));
+	memcpy(mesh[objectId + 2].mat.emissive, emissive, 4 * sizeof(float));
+	mesh[objectId + 2].mat.shininess = shininess;
+	mesh[objectId + 2].mat.texCount = texcount;
 
 	createCube(objectId);
+	createCone(objectId + 1, 1.0f, 0.5f, 12);
+	createCone(objectId + 2, 1.0f, 0.5f, 12);
 	*addedToId = addToId;
 }
 
@@ -55,6 +68,7 @@ void Spaceship::draw(VSShaderLib _shader) {
 	pushMatrix(MODEL);
 
 	GLint loc;
+	//CUBE
 	// send the material
 	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.ambient");
 	glUniform4fv(loc, 1, mesh[objectId].mat.ambient);
@@ -78,10 +92,61 @@ void Spaceship::draw(VSShaderLib _shader) {
 	glUniformMatrix4fv(pvm_uniformId, 1, GL_FALSE, mCompMatrix[PROJ_VIEW_MODEL]);
 	computeNormalMatrix3x3();
 	glUniformMatrix3fv(normal_uniformId, 1, GL_FALSE, mNormal3x3);
-
 	// Render mesh
 	glBindVertexArray(mesh[objectId].vao);
 	glDrawElements(mesh[objectId].type, mesh[objectId].numIndexes, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+	
+	// CONE1
+	// send the material
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.ambient");
+	glUniform4fv(loc, 1, mesh[objectId + 1].mat.ambient);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.diffuse");
+	glUniform4fv(loc, 1, mesh[objectId + 1].mat.diffuse);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.specular");
+	glUniform4fv(loc, 1, mesh[objectId + 1].mat.specular);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.shininess");
+	glUniform1f(loc, mesh[objectId + 1].mat.shininess);
+	// send matrices to OGL
+	pushMatrix(MODEL);
+	translate(MODEL, 0.0f, 0.5f, 0.5f);
+	rotate(MODEL, 90.0f, 0.0f, 0.0f, 1.0f);
+	scale(MODEL, 1.0f, 1.0f, 1.0f);
+	computeDerivedMatrix(PROJ_VIEW_MODEL);
+	glUniformMatrix4fv(vm_uniformId, 1, GL_FALSE, mCompMatrix[VIEW_MODEL]);
+	glUniformMatrix4fv(pvm_uniformId, 1, GL_FALSE, mCompMatrix[PROJ_VIEW_MODEL]);
+	computeNormalMatrix3x3();
+	glUniformMatrix3fv(normal_uniformId, 1, GL_FALSE, mNormal3x3);
+	popMatrix(MODEL);
+	// Render mesh
+	glBindVertexArray(mesh[objectId + 1].vao);
+	glDrawElements(mesh[objectId + 1].type, mesh[objectId + 1].numIndexes, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	// CONE2
+	// send the material
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.ambient");
+	glUniform4fv(loc, 1, mesh[objectId + 2].mat.ambient);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.diffuse");
+	glUniform4fv(loc, 1, mesh[objectId + 2].mat.diffuse);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.specular");
+	glUniform4fv(loc, 1, mesh[objectId + 2].mat.specular);
+	loc = glGetUniformLocation(shader.getProgramIndex(), "mat.shininess");
+	glUniform1f(loc, mesh[objectId + 2].mat.shininess);
+	// send matrices to OGL
+	pushMatrix(MODEL);
+	translate(MODEL, 1.0f, 0.5f, 0.5f);
+	rotate(MODEL, 270.0f, 0.0f, 0.0f, 1.0f);
+	scale(MODEL, 1.0f, 1.0f, 1.0f);
+	computeDerivedMatrix(PROJ_VIEW_MODEL);
+	glUniformMatrix4fv(vm_uniformId, 1, GL_FALSE, mCompMatrix[VIEW_MODEL]);
+	glUniformMatrix4fv(pvm_uniformId, 1, GL_FALSE, mCompMatrix[PROJ_VIEW_MODEL]);
+	computeNormalMatrix3x3();
+	glUniformMatrix3fv(normal_uniformId, 1, GL_FALSE, mNormal3x3);
+	popMatrix(MODEL);
+	// Render mesh
+	glBindVertexArray(mesh[objectId + 2].vao);
+	glDrawElements(mesh[objectId + 2].type, mesh[objectId + 2].numIndexes, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 
 	popMatrix(MODEL);
